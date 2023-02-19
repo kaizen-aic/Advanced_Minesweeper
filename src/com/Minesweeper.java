0517e0aa7c5f18e7142689d798c8642ac6da6ec4
Kaizen <christopher.a.castillo.b@gmail.com>
2023-02-18 22:48:14 -0500

    Modified Zetcode Minesweeper with Higher Resolution Tiles

---
+++ src/com/Minesweeper.java
@@ -0,0 +1,40 @@
+package src.com;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Minesweeper extends JFrame {
+
+    private JLabel statusbar;
+
+    public Minesweeper() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        statusbar = new JLabel("");
+        add(statusbar, BorderLayout.NORTH);
+
+        add(new Board(statusbar));
+
+        setResizable(false);
+        pack();
+
+        setTitle("Advanced Minesweeper");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            Minesweeper ex = new Minesweeper();
+            ex.setVisible(true);
+        });
+    }
+}
